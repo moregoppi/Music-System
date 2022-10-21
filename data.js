600e548162f731551b79764338c3708e9c2b8674
Gurpreet-Tech-Coder <71172450+Gurpreet-Tech-Coder@users.noreply.github.com>
2022-10-22 02:06:10 +0530

    Add all my files

---
+++ data.js
@@ -0,0 +1,17 @@
+var song_list ={
+    "01":{
+        title: "Baby Keem",
+        artist: "",
+        genre: "Pop",
+        duration: "2.37",
+        track:"16.mp3",
+    },
+    "02":{
+        title: "Baby Keem",
+        artist: "",
+        genre: "Pop",
+        duration: "2.37",
+        track:"16.mp3",
+    },
+
+}
