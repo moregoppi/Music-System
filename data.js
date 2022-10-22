cfe4be2420fc2cb5095de34197c3a881fa1647db
Gurpreet-Tech-Coder <71172450+Gurpreet-Tech-Coder@users.noreply.github.com>
2022-10-22 14:24:43 +0530

    Add all my files

--- data.js
+++ data.js
@@ -1,17 +1,45 @@
 var song_list ={
     "01":{
-        title: "Baby Keem",
-        artist: "",
+        title: "16",
+        artist: "Baby Keen",
         genre: "Pop",
         duration: "2.37",
         track:"16.mp3",
     },
     "02":{
-        title: "Baby Keem",
-        artist: "",
+        title: "Honest",
+        artist: "Baby Keen",
         genre: "Pop",
-        duration: "2.37",
-        track:"16.mp3",
+        duration: "3.06",
+        track:"Honest.mp3",
+    },
+    "03":{
+        title: "Toosie Slide",
+        artist: "Drake",
+        genre: "Pop",
+        duration: "4.07",
+        track:"Toosie Slide.mp3",
+    },
+    "04":{
+        title: "Heave and Hell",
+        artist: "Kanye West",
+        genre: "Pop",
+        duration: "2.25",
+        track:"Heaven and Hell.mp3",
+    },
+    "05":{
+        title: "Mafia",
+        artist: "Travis Scott",
+        genre: "Pop",
+        duration: "4.01",
+        track:"MAFIA.mp3",
+    },
+    "06":{
+        title: "Baddal",
+        artist: "Jasmien Sandles",
+        genre: "Pop",
+        duration: "4.03",
+        track:"Baddal.mp3",
     },
 
 }
